70c86c3b2f9f1ec8926bcbf2df9841fe8ab0df4e
Mima2370 <michielvde@hotmail.be>
2025-07-29 17:23:08 +0200

    Clean up a bit

--- messages/AimModeMessage.py
+++
@@ -1,4 +0,0 @@
-import socketUtils
-
-async def handle_AimModeMessage(reader, writer, message):
-    await socketUtils.send_message_to_multiple_writers(message, writer.game.writers)
--- messages/MoveMessage.py
+++
@@ -1,7 +0,0 @@
-import socketUtils
-
-async def handle_MoveMessage(reader, writer, message):
-    for writer in writer.game.writers:
-        message2 = message.copy()
-        message2["id"] = writer.userId
-        await socketUtils.send_message(message2, writer)
